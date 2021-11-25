c5b5a5ba5b78b32cbd969c3ab56d9a9fe47d2c11
delvauxjulene <julene.delvaux@gmail.com>
2021-11-25 15:13:14 +0100

    modif

--- src/Form/MessageType.php
+++ src/Form/MessageType.php
@@ -12,7 +12,7 @@ class MessageType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('category', )
+            ->add('category')
             ->add('title')
             ->add('content')
         ;
